848f58e076a44dba52d3b15a49aab098cfc53e6c
Parkduksung <duksung1234@naver.com>
2022-09-06 22:28:38 +0900

    [feature] MVI 고찰

--- app/src/main/java/com/example/studymvi/MainViewModel.kt
+++ app/src/main/java/com/example/studymvi/MainViewModel.kt
@@ -3,11 +3,11 @@ package com.example.studymvi
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 
 class MainViewModel : ViewModel() {
-
 
     var state by mutableStateOf(MainViewState())
         private set
