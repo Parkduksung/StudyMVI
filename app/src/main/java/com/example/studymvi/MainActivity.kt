eee289a19f1fef7d95af47df37e87cceba6c835f
Parkduksung <duksung1234@naver.com>
2022-09-30 14:42:40 +0900

    [feature] Gradle 변경.

--- app/src/main/java/com/example/studymvi/MainActivity.kt
+++ app/src/main/java/com/example/studymvi/MainActivity.kt
@@ -11,36 +11,8 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
 
-            //todo 화면구성
-
-            //Result
-            //정확한 MVI 는 리엑트를 공부해봐야 할것 같으며
-            //내 생각엔 MVVM 에서 State 로 바꾸고
-            //State 의 value 값이 바뀌면
-            //recompose 가 일어나는걸 활용해서
-            //화면을 변화하는 패턴으로 인식된다.
-
-            //progress visibility
-            if (mainViewModel.state.isLoading) {
-
-            } else {
-
-            }
-
-            //Error 발생시.
-            mainViewModel.state.error?.let {
-
-            }
-
-            //성공시
-            mainViewModel.state.success?.let {
-
-            }
-
         }
-
     }
 }
