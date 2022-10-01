186f562ff3fb2ace7f36b71bda104a04e09c8dd0
Parkduksung <duksung1234@naver.com>
2022-10-01 23:02:37 +0900

    [feature]LiveData -> Flow

--- app/src/main/java/com/example/studymvi/MainActivity.kt
+++ app/src/main/java/com/example/studymvi/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.studymvi
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.material.Button
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.collectLatest
 
 class MainActivity : ComponentActivity() {
 
@@ -12,10 +15,23 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+
+        lifecycleScope.launchWhenCreated {
+            mainViewModel.uiState.collectLatest { uiState ->
+                when (uiState) {
+                    is UiState.Loading -> {
+                        Log.d("결과", "Loading")
+                    }
+                    is UiState.GetString -> {
+                        Log.d("결과", uiState.data)
+                    }
+                }
+            }
+        }
+
         setContent {
-
-            mainViewModel.uiStateMediator.observe(this) {}
-
             Button(
                 onClick = {
                     mainViewModel.clickButton()
--- app/src/main/java/com/example/studymvi/MainViewModel.kt
+++ app/src/main/java/com/example/studymvi/MainViewModel.kt
@@ -1,63 +1,26 @@
 package com.example.studymvi
 
-import android.util.Log
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import java.util.*
 
 
 class MainViewModel : ViewModel() {
 
-    var state by mutableStateOf(UiState())
-        private set
-
-    private val _uiStateMediator = MutableLiveData<UiState>()
-    val uiStateMediator = _uiStateMediator.switchMap { state ->
-        liveData {
-            state.subUiState1?.let { emit(emit1(it)) }
-            state.subUiState2?.let { emit(emit2(it)) }
-        }
-    }
+    private val _uiState = MutableStateFlow<UiState>(UiState.Loading)
+    val uiState : StateFlow<UiState> = _uiState
 
     fun clickButton() {
         viewModelScope.launch {
-            _uiStateMediator.value =
-                UiState(subUiState1 = SubUiState1.Sub1(Random().nextInt(100).toString()))
-            _uiStateMediator.value =
-                UiState(subUiState2 = SubUiState2.Sub2(Random().nextInt(100).toString()))
+            _uiState.value =UiState.GetString(data = Random().nextInt(100).toString())
         }
     }
-
-    private fun emit1(item: SubUiState1) {
-        Log.d("결과1", (item as SubUiState1.Sub1).data)
-    }
-
-    private fun emit2(item: SubUiState2?) {
-        Log.d("결과1", (item as SubUiState2.Sub2).data)
-    }
-
 }
 
-
-
-data class UiState(
-    var commonState: CommonState? = null,
-    var subUiState1: SubUiState1? = null,
-    var subUiState2: SubUiState2? = null
-)
-
-sealed class CommonState {
-    data class LoadingState(val isShow: Boolean)
-}
-
-sealed class SubUiState1 {
-    data class Sub1(val data: String) : SubUiState1()
-}
-
-sealed class SubUiState2 {
-    data class Sub2(val data: String) : SubUiState2()
+sealed class UiState {
+    data class GetString(val data : String) : UiState()
+    object Loading : UiState()
 }
 
