126961f0aab9dec40e4f84acd216ceab8000b107
Parkduksung <duksung1234@naver.com>
2023-04-11 23:11:53 +0900

    [feature] TextListContract 추가.

---
+++ feature-text/src/main/java/com/example/feature_text/TextListContract.kt
@@ -0,0 +1,20 @@
+package com.example.feature_text
+
+import com.example.core_common.UnidirectionalViewModel
+import com.example.core_database.room.entity.TextEntity
+
+interface TextListContract :
+    UnidirectionalViewModel<TextListContract.Event, TextListContract.State> {
+
+    data class State(
+        val textList: List<TextEntity> = emptyList(),
+        val totalTextCount: Int = 0,
+        val preferenceText: String = ""
+    )
+
+    sealed class Event {
+        data class InsertTextItem(val text: String) : Event()
+        data class DeleteTextEntity(val textEntity: TextEntity) : Event()
+    }
+
+}
