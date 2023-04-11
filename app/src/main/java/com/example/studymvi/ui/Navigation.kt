4acd1d2dd0227750f37658ac6f4c5bae8276ee6c
Parkduksung <duksung1234@naver.com>
2023-04-11 23:13:26 +0900

    [feature] 패키지이동.

--- app/src/main/java/com/example/studymvi/ui/Navigation.kt
+++ app/src/main/java/com/example/studymvi/ui/Navigation.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.feature_text.SampleScreen
+import com.example.feature_text.view.SampleScreen
 
 
 @Composable
